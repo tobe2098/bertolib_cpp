5ba9b246a2935128f57e5041cccaeea0dd669eea
Antoni Bertolin <tonibm1220@gmail.com>
2024-11-26 17:12:24 +0100

    Create berdss.h

---
+++ berdss.h
@@ -0,0 +1,12 @@
+#ifndef BERTOLIB_DATA_STRUCTURE_HEADER
+#define BERTOLIB_DATA_STRUCTURE_HEADER
+
+// MIN-heap with sorting function.
+// Hashmap
+// Set and derived from hashmap
+// dynamic array (void*)
+// double-linked-list
+// Binary search tree
+// Red-black tree
+
+#endif
